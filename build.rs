da35620bc9416ab2f5e89ce0f4f620b3d831d135
whs31 <riazantsev.dl@edu.spbstu.ru>
2025-09-23 00:03:52 +0300

    v0.1.12

--- build.rs
+++ build.rs
@@ -17,9 +17,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         "share/proto/quasar/image.proto",
         "share/proto/quasar/relay/datagrams/nav.proto",
         "share/proto/quasar/relay/datagrams/shell.proto",
+        "share/proto/quasar/relay/datagrams/status.proto",
         "share/proto/quasar/relay/services/license.proto",
         "share/proto/quasar/relay/services/nav.proto",
         "share/proto/quasar/relay/services/shell.proto",
+        "share/proto/quasar/relay/services/status.proto",
       ],
       &["share/proto"],
     )?;
