6e868b3065c7791c8d06edffc9458d81610137db
whs31 <kerbalspacefruit@gmail.com>
2025-12-22 18:30:51 +0300

    v0.3.3

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ from conan.tools.files import rmdir
 
 class QuasarProtobufsRecipe(ConanFile):
     name = "quasar.protobufs"
-    version = "0.3.2"
+    version = "0.3.3"
     description = "Protobufs pack related to QuaSAR"
     author = "whs31 <whs31@github.io>"
     topics = ("grpc", "protocol", "network")
